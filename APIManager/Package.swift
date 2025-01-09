d164f94c9fc93077de7bb4a6365be8fcab5a5de1
Nirav shekhaliya <nirav.shekhaliya@blobstation.com>
2025-01-09 18:45:13 +0530

    Update package file

--- APIManager/Package.swift
+++ APIManager/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "APIManager",
+    platforms: [
+           .iOS(.v12),  // Specify the minimum platform version
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
